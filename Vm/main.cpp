25696d37e74b161d66644ef5caf22def30b4860f
Jason Chen <j927chen@uwaterloo.ca>
2020-12-07 18:20:42 -0500

    Initial Commit

---
+++ Vm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Vm
+//
+//  Created by Jason Chen on 2020-12-07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
